52d308c6c77779f8e83f7240e14b488a187a3686
Daniel <36088889+Lotyan@users.noreply.github.com>
2022-05-18 15:20:00 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function(){
+
+    $("#header__menu").on("click","a", function (event) {
+
+        //отменяем стандартную обработку нажатия по ссылке
+
+        event.preventDefault();
+
+
+        //забираем идентификатор бока с атрибута href
+
+        var id  = $(this).attr('href'),
+
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+
+            top = $(id).offset().top;
+      
+
+        //анимируем переход на расстояние - top за 1500 мс
+
+        $('body,html').animate({scrollTop: top}, 1300);
+
+    });
+
+});
+
+$('.first__arrow-icon').on('click', function() {
+  $('html,body').animate({scrollTop:$('.about-me').offset().top+"px"},{duration:1E3});
+});
+
